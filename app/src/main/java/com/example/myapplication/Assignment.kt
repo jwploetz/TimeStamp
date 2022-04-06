4b4f086ece9215916448498a2627028fe3ee4f4b
Alex Anderson <chaoticgamer.exe@gmail.com>
2022-04-06 11:26:06 -0400

    functions added

--- app/src/main/java/com/example/myapplication/Assignment.kt
+++ app/src/main/java/com/example/myapplication/Assignment.kt
@@ -3,4 +3,10 @@ package com.example.myapplication
 class Assignment(Deadline:Long, AssingmentName: String) {
     var AssignmentDate = Deadline;
     var Name = AssingmentName;
+    fun ChangeAssignmentDate(x: Long){
+            AssignmentDate = x;
+    }
+    fun ChangeAssignmentName(x: String){
+            Name = x
+    }
 }
